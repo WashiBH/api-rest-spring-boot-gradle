ccdbcb88a4bc309dd4164fbb316e487d0e98bbbc
washi <washi1886@gmail.com>
2019-01-24 11:27:02 -0500

    first commit

---
+++ src/main/java/com/apirest/springboot/gradle/apirestspringbootgradle/ApiRestSpringBootGradleApplication.java
@@ -0,0 +1,14 @@
+package com.apirest.springboot.gradle.apirestspringbootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringBootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringBootGradleApplication.class, args);
+	}
+
+}
+
